0fe04463563478280ef0d15613581f60481c1ab7
ScottJ <westdalefarmer@gmail.com>
2026-01-12 13:52:11 -0500

    alignment

--- ProjectPrinter/devs.vb
+++ ProjectPrinter/devs.vb
@@ -703,7 +703,7 @@ Public Class devs
 
             If OS = OSType.OS_ZOS Then
                 Program.Log($"Setting page for Z/OS")
-                firstline = 43
+                firstline = 45
                 linesPerPage = 66
                 StartLine = 5
             End If
